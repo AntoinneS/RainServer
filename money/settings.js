52234473282fde40823a0f099df9c8fcf66b645c
Elijah Bandi <imdatbulll@gmail.com>
2014-02-01 13:36:57 -0600

    Create settings.js

---
+++ money/settings.js
@@ -0,0 +1,56 @@
+//Money settings
+
+exports.moneyOps = ['bandi','ifaze'];
+
+exports.casino = {
+on: false,
+auth: ['critch55'],
+roul: {
+on: false,
+multispin: false,
+multicolor: false,
+multibet: false
+},
+hol: {
+on: false,
+multibet: false,
+doubleupon: false
+},
+};
+
+exports.arcade = {
+auth: ['darknesssreigns'],
+hangman: {
+on: false
+},
+rps: {
+on: false,
+boton: false,
+ls: false,
+},
+tour: {
+on: false,
+randtier: false,
+commontier: false
+},
+};
+ 
+exports.automoney = {
+ifaze: 99999999999,
+bandi: 99999999999,
+};
+
+exports.autotkts = {
+ifaze: 99999999999,
+bandi: 99999999999,
+};
+
+exports.autobp = {
+ifaze: 99999999999,
+bandi: 99999999999,
+};
+
+exports.autocoins = {
+ifaze: 99999999999,
+bandi: 99999999999,
+};
